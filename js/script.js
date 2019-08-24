745565d11df6fc3474b1c16e73a541bf62beba44
Work <pumba@meta.ua>
2019-08-25 00:41:57 +0300

    score

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@
     var pipes = new Image();
     var xPos=400;
     var yPos=230;
-    var vxPos= 3;
-    var vyPos = 3;
+    var vxPos= 5;
+    var vyPos = 5;
     var player2Xpos = 890;
     var player2YPos = 300;
     var player1XPos =0;
     var player1YPos =200;
+    var scorePlayer1 = 0;
+    var scorePlayer2 = 0;
     document.addEventListener('mousemove', playerMove);
     player1.src ="img/timon.png";
     player2.src ="img/pumba.png";
@@ -48,16 +50,20 @@
     }
 // Обработка пересечения с полями
     function wall() {
-    if (xPos + ball.width <= play.width && yPos + ball.height+vyPos < play.height &&   xPos >= 0 && yPos + vyPos > 0 ) { //alert(xPos + "," + yPos + "," + vxPos + "," + vyPos + "," + ",№1");
+    if (xPos + ball.width <= play.width && yPos + ball.height+vyPos < play.height &&   xPos >= 0 && yPos + vyPos > 0 ) {
         vxPos =    vxPos;
         vyPos =    vyPos;
         // строка для отладки alert(xPos + "," + yPos + "," + vxPos + "," + vyPos + "," + play.width + "," + play.height + ",№1");
     } else if ( xPos + ball.width >= play.width && vxPos > 0 ) {
         vxPos =  - vxPos;
         vyPos =    vyPos;
+        scorePlayer2++;
+        console.log("ГОЛ Пумбе! " + scorePlayer2);
     } else if (xPos <= 0 && vyPos < 0 && vxPos < 0 ){
         vxPos =  - vxPos;
         vyPos =    vyPos;
+        scorePlayer1++;
+        console.log("ГОЛ Тимону! " + scorePlayer1);
     } else if (xPos <= 0 && vyPos > 0 && vxPos < 0 ){
         vxPos =  - vxPos;
         vyPos =    vyPos;
@@ -69,53 +75,22 @@
     player2Xpos + player2.width > xPos && player2Xpos < xPos + ball.width && player2YPos + player2.height > yPos && player2YPos < yPos + ball.height && vxPos>0) {
         vxPos =  - vxPos;
     }
-    /*if ((collision(player1, ball) && vxPos<0) || (collision(player2, ball) && vxPos>0)){
-        vxPos =  - vxPos;
-    }*/
     }
 
 //движение "ИИ"
     function aiMove() {
         var y;
-        var vY;
+        var vY= Math.abs(vyPos) + 1;
         // делаем скорость оппонента зависимой от скорости шарика
-        switch (vyPos) {
-            case 2:
-                vY = 2;
-                break;
-            case 3:
-                vY = 3;
-                break;
-            case 4:
-                vY = 4;
-                break;
-            case 5:
-                vY = 5;
-                break;
-            case 6:
-                vY = 5;
-                break;
-            case 7:
-                vY = 6;
-                break;
-            case 8:
-                vY = 6;
-                break;
-            case 9:
-                vY = 7;
-                break;
-            case 0:
-                vY = 0;
-                break;
-        }
 
-        if (yPos < player2YPos + player2.height / 2) {
+
+        if (yPos < player2YPos - player2.height / 2) {
             y = player2YPos - vY;
         }
         if (yPos > player2YPos + player2.height / 2) {
             y = player2YPos + vY;
         }
-        if (10 < y && y < play.height - player2.height - 5) {
+        if (0 < y && y < play.height - player2.height - 0) {
             player2YPos = y;
         }
     }
